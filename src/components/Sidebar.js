df23d3c3497cb3bf26de4f77cc30c80e0c86c1cd
Zahir Al-Asad <zahiralasad@yahoo.com>
2024-02-26 23:56:28 +0100

    sample Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,11 +3,7 @@ import React from 'react';
 function Sidebar(){
     return (
       <div className="Sidebar">
-      <div className="mt-2 p-4 p-md-5 mb-4 rounded text-bg-dark">
-        <div className="p-2 border border-white rounded text-center">
           <h3>Sidebar</h3>
-        </div>
-      </div>
     </div >
       )
 }
