2d60ef82ebe334e75f64c9a555f51d619dd09d88
Zahir Al-Asad <zahiralasad@yahoo.com>
2024-02-26 11:20:21 +0100

    added the side bar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Sidebar(){
+    return (
+      <div className="Sidebar">
+      <div className="mt-2 p-4 p-md-5 mb-4 rounded text-bg-dark">
+        <div className="p-2 border border-white rounded text-center">
+          <h3>Sidebar</h3>
+        </div>
+      </div>
+    </div >
+      )
+}
+export default Sidebar;
