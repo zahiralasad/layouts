f64e6508386b3e3920fae08df1390968b1ece13f
zahiralasad <zahiralasad@yahoo.com>
2024-02-27 16:44:04 +0100

    add logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import SlideShow from "./Slideshow";
 import Cards from './Cards'
 import '../css/App.css';
+import logo from '../images/logo.png';
 
 
 import { Link } from "react-router-dom";
@@ -10,7 +11,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div >
-
+      <img src={logo} className="bi me-2 text-white" width="50" height="50" alt="logo" />
       home
     </div>
   )
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as FarIcons from "react-icons/fa";
 
+
 function Sidebar(){
     return (
         <div className="Sidebar">
             <Link to='#' className=''>
                 <FarIcons.FaBars/>
             </Link>
+
           <h3>Sidebar</h3>
         </div >
     )
